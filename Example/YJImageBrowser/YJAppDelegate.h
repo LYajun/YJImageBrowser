acc13e4ca438fdf093adacbc946d3f77e0878fb1
刘亚军 <liuyajun1999@icloud.com>
2019-07-22 11:24:42 +0800

    Initial commit

---
+++ Example/YJImageBrowser/YJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YJAppDelegate.h
+//  YJImageBrowser
+//
+//  Created by lyj on 07/22/2019.
+//  Copyright (c) 2019 lyj. All rights reserved.
+//
+
+@import UIKit;
+
+@interface YJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/YJImageBrowser/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  YJImageBrowser
+//
+//  Created by lyj on 07/22/2019.
+//  Copyright (c) 2019 lyj. All rights reserved.
+//
+
+@import UIKit;
+#import "YJAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YJAppDelegate class]));
+    }
+}
